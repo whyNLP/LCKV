d42b8cfe18c292cb73d787ef308b06d35b2a574a
whywhy-rtx3090 <wuhy1@shanghaitech.edu.cn>
2023-12-09 20:35:57 +0800

    fix: bug on rotary

--- models/llama_fused_rotary.py
+++ models/llama_fused_rotary.py
@@ -65,6 +65,12 @@ def fused_apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
     return q_embed, k_embed
 
 def fused_apply_rotary_pos_emb_q(q, cos, sin, position_ids, unsqueeze_dim=1):
+    assert unsqueeze_dim == 1, "fused rotary pos emb only supports unsqueeze_dim=1"
+    assert q.shape[-1] == cos.shape[-1]*2, "q and cos must have the same embedding dimension"
+    if len(position_ids.shape) == 2:
+        assert position_ids.shape[0] == 1, "position_ids must have a batch dimension of 1"
+        position_ids = position_ids[0]
+    
     cos = cos[position_ids]
     sin = sin[position_ids]
     q_embed = apply_rotary_emb(q.transpose(1, 2), cos, sin).transpose(1, 2)
