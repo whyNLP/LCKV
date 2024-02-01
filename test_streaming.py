0812f5cb1d3dfcc6f6c6a974d21894f3ec610d19
whywhy-rtx3090 <wuhy1@shanghaitech.edu.cn>
2024-02-01 15:45:00 +0800

    fix: test streaming

--- test_streaming.py
+++ test_streaming.py
@@ -6,74 +6,104 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 from torch.nn import CrossEntropyLoss
 from streaming_llm.kv_cache import StartRecentKVCache
-from streaming_llm.utils import parse_args, load
+from streaming_llm.enable_streaming_llm import enable_streaming_llm
+import argparse
 
-device = "cuda"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name_or_path", type=str, default="TinyLlama/TinyLlama-1.1B-intermediate-step-1195k-token-2.5T"
+    )
+    parser.add_argument("--revision", type=str, default="main")
+    parser.add_argument("--tokenizer_name_or_path", type=str, default=None)
+    parser.add_argument("--dataset_name", type=str, default=None)
+    parser.add_argument("--task", type=str, default=None)
+    parser.add_argument(
+        "--split", type=str, default="test", choices=["validation", "test"]
+    )
+    parser.add_argument(
+        "--streaming", action="store_true", help="enable streaming download"
+    )
+
+    parser.add_argument(
+        "--num_samples",
+        type=int,
+        default=None,
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="streaming/debug",
+    )
+
+    parser.add_argument(
+        "--torch_dtype",
+        type=str,
+        default="bfloat16",
+    )
+
+    parser.add_argument("--enable_streaming", action="store_true")
+    parser.add_argument("--start_size", type=int, default=1)
+    parser.add_argument("--recent_size", type=int, default=255)
+
+    parser.add_argument("--num_eval_tokens", type=int, default=None)
+
+    args = parser.parse_args()
+    return args
+
+def load(model_name_or_path, torch_dtype):
+    print(f"Loading model from {model_name_or_path} ...")
+    # however, tensor parallel for running falcon will occur bugs
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name_or_path,
+        trust_remote_code=True,
+    )
+    torch_dtype = (
+        torch_dtype
+        if torch_dtype in ["auto", None]
+        else getattr(torch, torch_dtype)
+    )
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name_or_path,
+        device_map="auto",
+        torch_dtype=torch_dtype,
+        trust_remote_code=True,
+    )
+    if tokenizer.pad_token_id is None:
+        if tokenizer.eos_token_id is not None:
+            tokenizer.pad_token_id = tokenizer.eos_token_id
+        else:
+            tokenizer.pad_token_id = 0
+    model.eval()
+    return model, tokenizer
 
 args = parse_args()
 
-data = load_dataset(args.dataset_name, args.task, split=args.split)
+data = load_dataset(args.dataset_name, args.task, split=args.split, streaming=args.streaming)
+if args.num_samples is not None:
+    data = data.select(range(args.num_samples))
 
-model, tokenizer = load(args.model_name_or_path)
+model, tokenizer = load(args.model_name_or_path, args.torch_dtype)
 
 nlls = []
 loss_fn = CrossEntropyLoss(reduction="none")
 past_key_values = None
 
-if args.enable_start_recent_kv_cache:
-    if "llama" in model.config.model_type:
-        k_seq_dim = v_seq_dim = 2
-    elif "mpt" in model.config.model_type:
-        v_seq_dim = 2
-        k_seq_dim = 3
-    elif "pythia" in model.config.model_type:
-        k_seq_dim = v_seq_dim = 2
-    elif "falcon" in model.config.model_type:
-        v_seq_dim = 1
-        k_seq_dim = 1
-    else:
-        raise ValueError(f"got {model.config.model_type}")
-    kv_cache = StartRecentKVCache(
-        start_size=args.start_size,
-        recent_size=args.recent_size,
-        k_seq_dim=k_seq_dim,
-        v_seq_dim=v_seq_dim,
+if args.enable_streaming:
+    kv_cache = enable_streaming_llm(
+        model, start_size=args.start_size, recent_size=args.recent_size
     )
 else:
     kv_cache = None
-
-if args.enable_pos_shift:
-    if "opt-llama" in model.config.model_type:
-        from models.modeling_llama_opt_streaming import enable_opt_llama_pos_shift_attention
-
-        enable_opt_llama_pos_shift_attention(model)
-    elif "llama" in model.config.model_type:
-        from streaming_llm.pos_shift.modify_llama import enable_llama_pos_shift_attention
-
-        enable_llama_pos_shift_attention(model)
-    elif "falcon" in model.config.model_type:
-        from streaming_llm.pos_shift.modify_falcon import (
-            enable_falcon_pos_shift_attention,
-        )
-
-        enable_falcon_pos_shift_attention(model)
-    elif "gpt_neox" in model.config.model_type:
-        from streaming_llm.pos_shift.modify_gpt_neox import (
-            enable_gpt_neox_pos_shift_attention,
-        )
-
-        enable_gpt_neox_pos_shift_attention(model)
-    elif "mpt" in model.config.model_type:
-        pass
-    else:
-        raise ValueError(f"got {model.config.model_type}")
-
 
 os.makedirs(args.output_dir, exist_ok=True)
 with open(f"{args.output_dir}/log.txt", "w") as f:
 
     num_eval_tokens = 0
-    for text in data["text"][: args.num_samples]:
+    for text in data["text"]:
         encodings = tokenizer(text, return_tensors="pt")
 
         print(encodings.input_ids[:, :10])
