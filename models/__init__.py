90221e5fb306e8851d544df7ff5a7d577e523818
whywhy-rtx3090 <wuhy1@shanghaitech.edu.cn>
2024-03-10 09:41:41 +0800

    feat: remove irrelevant codes

--- models/__init__.py
+++ models/__init__.py
@@ -1,64 +1,26 @@
-from .configuration_gpt2 import GPT2Config, ALGPT2Config, CycleGPT2Config
-from .modeling_gpt2 import GPT2LMHeadModel, ALGPT2LMHeadModel, CycleGPT2LMHeadModel
-from .configuration_llama import BestLlamaConfig
-from .modeling_llama import LlamaForCausalLM
 from .configuration_llama import OptLlamaConfig
 from .modeling_llama_opt import LlamaForCausalLM as OptLlamaForCausalLM
-from .configuration_llama import ALLlamaConfig, CycleLlamaConfig
-from .modeling_alllama import ALLlamaForCausalLM, CycleLlamaForCausalLM
-from .configuration_llama import KVLlamaConfig, HiddenLlamaConfig
-from .modeling_llamakv import LlamaKVForCausalLM, LlamaHiddenForCausalLM
 from .wandb_callback import WandbCallback
 
-from transformers import CONFIG_MAPPING, MODEL_FOR_CAUSAL_LM_MAPPING
-CONFIG_MAPPING.register("gpt2", GPT2Config, exist_ok = True)
-MODEL_FOR_CAUSAL_LM_MAPPING.register(GPT2Config, GPT2LMHeadModel, exist_ok = True)
-CONFIG_MAPPING.register("algpt2", ALGPT2Config)
-MODEL_FOR_CAUSAL_LM_MAPPING.register(ALGPT2Config, ALGPT2LMHeadModel)
-CONFIG_MAPPING.register("cyclegpt2", CycleGPT2Config)
-MODEL_FOR_CAUSAL_LM_MAPPING.register(CycleGPT2Config, CycleGPT2LMHeadModel)
-CONFIG_MAPPING.register("best-llama", BestLlamaConfig)
-MODEL_FOR_CAUSAL_LM_MAPPING.register(BestLlamaConfig, LlamaForCausalLM)
-
 from transformers import AutoConfig, AutoModelForCausalLM
-AutoConfig.register("alllama", ALLlamaConfig)
-AutoModelForCausalLM.register(ALLlamaConfig, ALLlamaForCausalLM)
-AutoConfig.register("cyclellama", CycleLlamaConfig)
-AutoModelForCausalLM.register(CycleLlamaConfig, CycleLlamaForCausalLM)
-AutoConfig.register("kv-llama", KVLlamaConfig)
-AutoModelForCausalLM.register(KVLlamaConfig, LlamaKVForCausalLM)
-AutoConfig.register("hidden-llama", HiddenLlamaConfig)
-AutoModelForCausalLM.register(HiddenLlamaConfig, LlamaHiddenForCausalLM)
 AutoConfig.register("opt-llama", OptLlamaConfig)
 AutoModelForCausalLM.register(OptLlamaConfig, OptLlamaForCausalLM)
 
 import os
-if os.environ.get('ALGPT_FLASH_ATTN', False):
-    import transformers
-    from .gpt2_flash_attention import forward
-    transformers.models.gpt2.modeling_gpt2.GPT2Attention.forward = forward
 
 if os.environ.get('ALGPT_FUSED_RMSNORM', False):
     import transformers
     from flash_attn.ops.rms_norm import RMSNorm
     transformers.models.llama.modeling_llama.LlamaRMSNorm = RMSNorm
-    from . import modeling_llama
-    modeling_llama.LlamaRMSNorm = RMSNorm
     from . import modeling_llama_opt
     modeling_llama_opt.LlamaRMSNorm = RMSNorm
-    from . import modeling_alllama
-    modeling_alllama.LlamaRMSNorm = RMSNorm
 
 if os.environ.get('ALGPT_FUSED_CROSSENTROPY', False):
     import transformers
     from flash_attn.losses.cross_entropy import CrossEntropyLoss
     transformers.models.llama.modeling_llama.CrossEntropyLoss = CrossEntropyLoss
-    from . import modeling_llama
-    modeling_llama.CrossEntropyLoss = CrossEntropyLoss
     from . import modeling_llama_opt
     modeling_llama_opt.CrossEntropyLoss = CrossEntropyLoss
-    from . import modeling_alllama
-    modeling_alllama.CrossEntropyLoss = CrossEntropyLoss
 
 if os.environ.get('ALGPT_FUSED_ROTARY', False):
     import transformers
@@ -74,10 +36,6 @@ if os.environ.get('ALGPT_FUSED_ROTARY', False):
     transformers.models.llama.modeling_llama.LlamaLinearScalingRotaryEmbedding = LlamaLinearScalingRotaryEmbedding
     transformers.models.llama.modeling_llama.LlamaDynamicNTKScalingRotaryEmbedding = LlamaDynamicNTKScalingRotaryEmbedding
 
-    from . import modeling_llama
-    modeling_llama.apply_rotary_pos_emb = fused_apply_rotary_pos_emb
-    modeling_llama.apply_rotary_pos_emb_q = fused_apply_rotary_pos_emb_q
-
     from . import modeling_llama_opt
     modeling_llama_opt.apply_rotary_pos_emb = fused_apply_rotary_pos_emb
     modeling_llama_opt.apply_rotary_pos_emb_q = fused_apply_rotary_pos_emb_q
@@ -85,14 +43,9 @@ if os.environ.get('ALGPT_FUSED_ROTARY', False):
     from . import modeling_llama_opt_streaming
     modeling_llama_opt_streaming.apply_rotary_pos_emb_q = fused_apply_rotary_pos_emb_q
 
-    from . import modeling_llamakv
-    modeling_llamakv.apply_rotary_pos_emb_q = fused_apply_rotary_pos_emb_q
-
 if os.environ.get('ALGPT_FUSED_SWIGLU', False):
     import transformers
     from .llama_fused_swiglu import LlamaMLP
-    from . import modeling_llama
-    modeling_llama.LlamaMLP = LlamaMLP
     transformers.models.llama.modeling_llama.LlamaMLP = LlamaMLP
     from . import modeling_llama_opt
     modeling_llama_opt.LlamaMLP = LlamaMLP
--- models/configuration_llama.py
+++ models/configuration_llama.py
@@ -20,64 +20,6 @@
 """ LLaMA model configuration"""
 from transformers.models.llama.configuration_llama import LlamaConfig as _LlamaConfig
 
-
-class BestLlamaConfig(_LlamaConfig):
-
-    model_type = "best-llama"
-
-    def __init__(
-        self,
-        kv_pattern: str = "use_kv",
-        train_encoder: bool = True,
-        train_last_encoder: str = "none",
-        train_kv: bool = False,
-        mask_diagonal: bool = False,
-        num_encoders: int = 1,
-        num_warmup_layers: int = 0,
-        **kwargs,
-    ):
-        """
-        Args:
-            kv_pattern (`str`, *optional*, defaults to "use_kv"):
-                The pattern to use key-value during inference. The value should be one of 
-                "use_kv", "proj_kv", "use_hidden".
-                - "use_kv": use the key-value pair in the last layer as the kv cache in all
-                    transformer blocks.
-                - "proj_kv": use the key-value pair in the last layer as the kv cache in all
-                    transformer blocks, but will pass a trainable projection to simulate the
-                    original kv.
-                - "use_hidden": use the hidden vector in the last layer to recompute the
-                    key-value pair in each transformer blocks.
-            train_encoder (`bool`, *optional*, defaults to True):
-                Whether to train the encoder. If set to False, the encoder will be detached
-                from the computation graph when calculating the gradients.
-            train_last_encoder (`str`, *optional*, defaults to "none"):
-                Whether to train the last encoder. The value should be one of "none",
-                "encoder", "kv".
-            mask_diagonal (`bool`, *optional*, defaults to False):
-                Whether to mask the diagonal of the attention matrix. This is useful when
-                the model is used for sequence generation. The training and inference
-                should be consistent.
-            num_encoders (`int`, *optional*, defaults to 1):
-                The number of encoders. x encoders will ensure the starting x tokens in
-                prediction is consistent with training.
-            num_warmup_layers (`int`, *optional*, defaults to 0):
-                The number of transformer blocks that will use the key-value pair in the
-                original layers as the kv cache. The rest of the transformer blocks will
-                use the key-value pair in the last layer as the kv cache.
-        """
-        super().__init__(**kwargs)
-        self.kv_pattern = kv_pattern
-        self.train_encoder = train_encoder
-        self.train_last_encoder = train_last_encoder
-        self.train_kv = train_kv
-        self.mask_diagonal = mask_diagonal
-        self.num_encoders = num_encoders
-        self.num_warmup_layers = num_warmup_layers
-
-        if self.train_encoder:
-            assert self.train_last_encoder == "none", "Train encoder enabled. train_last_encoder must be 'none'."
-
 class OptLlamaConfig(_LlamaConfig):
 
     model_type = "opt-llama"
@@ -99,7 +41,7 @@ class OptLlamaConfig(_LlamaConfig):
                 "encoder" or an integer for the number of encoders to train.
             num_trained_encoders (`int`, *optional*, defaults to 1):
                 Number of encoders to train. The last `num_trained_encoders` will be
-                trained.
+                trained. Equivlent to `b-1` in the paper.
             train_kv (`bool`, *optional*, defaults to False):
                 Whether to train the key-value pair. If set to True, the loss will be
                 added with the MSE loss of the key-value pair in the last layer before
@@ -107,15 +49,15 @@ class OptLlamaConfig(_LlamaConfig):
                 training and inference will be consistent, but hurts the performance.
             num_encoders (`int`, *optional*, defaults to 8):
                 The number of encoders. x encoders will ensure the starting x tokens in
-                prediction is consistent with training.
+                prediction is consistent with training. Equivlent to `m+b-1` in the paper.
             num_warmup_layers (`int`, *optional*, defaults to 0):
                 Deprecated. Use `layer_types` and `target_layer` instead.
                 The number of transformer blocks that will use the key-value pair in the
                 original layers as the kv cache. The rest of the transformer blocks will
                 use the key-value pair in the last layer as the kv cache.
             layer_types (`str`, *optional*, defaults to ""):
-                The type of each layer. The value should be a comma separated string of
-                integers. The value "0" means the layer will use the key-value pair in
+                The type of each layer. The value should be a underscore separated string
+                of integers. The value "0" means the layer will use the key-value pair in
                 the original layers as the kv cache. The value "1" means the layer will
                 use the key-value pair in (possibly) the last layer as the kv cache. 
                 The value "2" means the layer will use the key-value pair during training,
@@ -164,166 +106,3 @@ class OptLlamaConfig(_LlamaConfig):
                 raise ValueError("The target layer should be the layer of type 2.")
         if layer_types.count(2) > 1:
             raise ValueError("Only one layer can be type 2.")
-
-
-class KVLlamaConfig(_LlamaConfig):
-
-    model_type = "kv-llama"
-
-    def __init__(
-        self,
-        num_warmup_layers: int = 0,
-        loss_weights: str = None,
-        mask_diagonal: bool = False,
-        **kwargs,
-    ):
-        """
-        Args:
-            num_warmup_layers (`int`, *optional*, defaults to 0):
-                The number of transformer blocks that will use the key-value pair in the
-                original layers as the kv cache. The rest of the transformer blocks will
-                use the key-value pair in the last layer as the kv cache.
-        """
-        super().__init__(**kwargs)
-        self.kv_pattern = "proj_kv"
-        self.mask_diagonal = mask_diagonal
-        self.num_warmup_layers = num_warmup_layers
-        self.loss_weights = loss_weights
-
-        if self.loss_weights is not None:
-            loss_weights = [float(x) for x in self.loss_weights.split("_")]
-            if len(self.loss_weights) != self.num_hidden_layers - self.num_warmup_layers:
-                raise ValueError("The number of loss weights should be equal to the number of hidden layers.")
-
-class HiddenLlamaConfig(_LlamaConfig):
-
-    model_type = "hidden-llama"
-
-    def __init__(
-        self,
-        num_warmup_layers: int = 0,
-        target_type: str = "hidden",
-        loss_weights: str = "none",
-        mask_diagonal: bool = False,
-        **kwargs,
-    ):
-        """
-        Args:
-            num_warmup_layers (`int`, *optional*, defaults to 0):
-                The number of transformer blocks that will use the key-value pair in the
-                original layers as the kv cache. The rest of the transformer blocks will
-                use the key-value pair in the last layer as the kv cache.
-            target_type (`str`, *optional*, defaults to "hidden"):
-                The loss to calculate additional to KV cache. Options: 'hidden',
-                'logits' and 'outputs'.
-        """
-        super().__init__(**kwargs)
-        self.kv_pattern = "use_kv"
-        self.mask_diagonal = mask_diagonal
-        self.num_warmup_layers = num_warmup_layers
-        self.target_type = target_type
-        self.loss_weights = loss_weights
-
-        if self.target_type not in ("hidden", "logits", "outputs"):
-            raise ValueError("target_type should be one of 'hidden', 'logits' and 'outputs'.")
-        if self.loss_weights not in ("none", None):
-            loss_weights = [float(x) for x in self.loss_weights.split("_")]
-            if len(loss_weights) != 2:
-                raise ValueError("The number of loss weights should be equal to 2.")
-
-
-class LlamaConfigBase(_LlamaConfig):
-    def __init__(
-        self,
-        use_sweet: bool = False,
-        use_ln_head: bool = True,
-        share_head: bool = False,
-        exit_strategy: str = "confidence",
-        loss_layers: str = "-1",
-        loss_weights: str = "1.0",
-        exit_layers: str = "-1",
-        exit_threshold: float = 1.0,
-        **kwargs
-    ):
-        """
-        Args:
-            use_sweet (`bool`, *optional*, defaults to False):
-                Use mutli-model strategy introduced in *Finding the SWEET Spot: Analysis
-                and Improvement of Adaptive Inference in Low Resource Settings* 
-                (https://aclanthology.org/2023.acl-long.829). The gredient in later blocks
-                will not affect former blocks.
-            use_ln_head (`bool`, *optional*, defaults to True):
-                Add a layer norm before each prediction head. This is consistent with GPT2
-                structure, which has a layer norm at the end of transformer blocks.
-            share_head (`bool`, *optional*, defaults to False):
-                Share the same prediction head for all exit transformer blocks. Suitable
-                for ALGPT2 which shares parameters for all transformer blocks.
-            exit_strategy (`str`, *optional*, defaults to "confidence"):
-                The strategy to exit early. The value should be one of "confidence",
-                "similarity".
-                - "confidence": exit when the model is confident enough. The model will
-                    exit when the probability of the predicted token is greater than the
-                    threshold.
-                - "similarity": exit when the model is similar enough. The model will
-                    exit when the cosine similarity between the hidden state in the 
-                    current layer and the hidden state in the last layer is greater than
-                    the threshold.
-                - "softmax": we take the difference between the top two values of softmax.
-                    If the difference is greater than the threshold, we exit.
-            loss_layers (`str`, *optional*, defaults to "-1"):
-                The layers to calculate loss. The layers are separated by underscore. The
-                default value is "-1", which means the last layer. The value "-2_-1" means
-                the last two layers.
-            loss_weights (`str`, *optional*, defaults to "1.0"):
-                The weights of loss layers. The weights are separated by underscore. The
-                default value is "1.0", which means the weight of the last layer is 1.0.
-                The number of weights should be equal to the number of loss layers.
-            exit_layers (`str`, *optional*, defaults to "-1"):
-                The layers to exit early. The layers are separated by underscore. The
-                default value is "-1", which means the last layer. The value "-2_-1" means
-                the last two layers.
-            exit_threshold (`float`, *optional*, defaults to 1.0):
-                The threshold to exit early. The value should be in (0, 1]. The default
-                value is 1.0, which means the model will not exit early.
-        """
-        super().__init__(**kwargs)
-        self.use_sweet = use_sweet
-        self.use_ln_head = use_ln_head
-        self.share_head = share_head
-        self.exit_strategy = exit_strategy
-        self.loss_layers = loss_layers
-        self.loss_weights = loss_weights
-        self.exit_layers = exit_layers
-        self.exit_threshold = exit_threshold
-
-        # post init check
-        loss_layers = [int(x) if int(x) >= 0 else int(x) + self.num_hidden_layers for x in self.loss_layers.split("_")]
-        loss_weights = [float(x) for x in self.loss_weights.split("_")]
-        exit_layers = [int(x) if int(x) >= 0 else int(x) + self.num_hidden_layers for x in self.exit_layers.split("_")]
-        if len(loss_layers) != len(loss_weights):
-            raise ValueError("The number of loss layers should be equal to the number of loss weights.")
-        if self.exit_threshold <= 0 or self.exit_threshold > 1:
-            raise ValueError("The exit threshold should be in (0, 1].")
-        if (self.num_hidden_layers - 1) not in exit_layers: # XXX: is this necessary?
-            raise ValueError("The last layer should be in the exit layers.")
-        if (self.num_hidden_layers - 1) not in loss_layers:
-            raise ValueError("The last layer should be in the loss layers.")
-        
-class LlamaConfig(LlamaConfigBase):
-    model_type = "llama"
-
-class ALLlamaConfig(LlamaConfigBase):
-    model_type = "alllama"
-
-class CycleLlamaConfig(LlamaConfigBase):
-    model_type = "cyclellama"
-
-    def __init__(self, cycles: int = 2, **kwargs):
-        """
-        Args:
-            cycles (`int`, *optional*, defaults to 2):
-                The number of cycles to run the model for. Each cycle will have (# of layers
-                / # of cycles) Transformer blocks.
-        """
-        super().__init__(**kwargs)
-        self.cycles = cycles
