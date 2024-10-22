5e686ef38228c0dd572eaf0774e070b1ea6ee152
whywhy-rtx3090 <43395692+why-in-Shanghaitech@users.noreply.github.com>
2024-10-22 15:24:58 +0800

    feat: impl of dialog streaming

--- run_streaming.py
+++ run_streaming.py
@@ -1,151 +1,127 @@
 import warnings
-
 warnings.filterwarnings("ignore")
+
+from pathlib import Path
+import requests
 
 import torch
 import argparse
 import json
 import os
-import time
-import re
-import sys
 
 import models
 
-from tqdm import tqdm
-from streaming_llm.utils import download_url, load_jsonl
-from streaming_llm.enable_streaming_llm import enable_streaming_llm
+from transformers.cache_utils import SinkCache
+from transformers.generation.streamers import TextStreamer
 from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
+    logging
 )
 
+logging.get_logger("transformers.tokenization_utils").setLevel(logging.ERROR)
+logging.get_logger("transformers.generation.utils").setLevel(logging.ERROR)
+logging.get_logger("transformers.models.llama.modeling_llama").setLevel(logging.ERROR)
 
-@torch.no_grad()
-def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
-    outputs = model(
-        input_ids=input_ids,
-        past_key_values=past_key_values,
-        use_cache=True,
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name_or_path", type=str, default="lmsys/vicuna-13b-v1.3")
+    parser.add_argument("--data_root", type=str, default="data/")
+
+    # generation related arguments
+    parser.add_argument("--length", type=int, default=1000)
+
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=1.0,
+        help="temperature of 1.0 has no effect, lower tend toward greedy sampling",
     )
-    past_key_values = outputs.past_key_values
-    pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
-    generated_ids = [pred_token_idx.item()]
-    pos = 0
-    for _ in range(max_gen_len - 1):
-        outputs = model(
-            input_ids=pred_token_idx,
-            past_key_values=past_key_values,
-            use_cache=True,
-        )
-        past_key_values = outputs.past_key_values
-        pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
-        generated_ids.append(pred_token_idx.item())
-        generated_text = (
-            tokenizer.decode(
-                generated_ids,
-                skip_special_tokens=True,
-                clean_up_tokenization_spaces=True,
-                spaces_between_special_tokens=False,
-            )
-            .strip()
-            .split(" ")
-        )
+    parser.add_argument(
+        "--repetition_penalty", type=float, default=1.0, help="primarily useful for CTRL model; in that case, use 1.2"
+    )
+    parser.add_argument("--k", type=int, default=0)
+    parser.add_argument("--p", type=float, default=0.9)
+    
+    # sink cache related arguments
+    parser.add_argument("--sink_cache", action="store_true", help="Whether to use sink cache.")
+    parser.add_argument("--window_length", type=int, default=256, help="Window size for sink cache.")
+    parser.add_argument("--num_sink_tokens", type=int, default=2, help="Number of sink tokens.")
 
-        now = len(generated_text) - 1
-        if now > pos:
-            print(" ".join(generated_text[pos:now]), end=" ", flush=True)
-            pos = now
+    args = parser.parse_args()
 
-        if pred_token_idx == tokenizer.eos_token_id:
-            break
-    print(" ".join(generated_text[pos:]), flush=True)
-    return past_key_values
-
-
-@torch.no_grad()
-def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=1000):
-    past_key_values = None
-    for idx, prompt in enumerate(prompts):
-        prompt = "USER: " + prompt + "\n\nASSISTANT: "
-        print("\n" + prompt, end="")
-        input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-        input_ids = input_ids.to(model.device)
-        seq_len = input_ids.shape[1]
-        if kv_cache is not None:
-            space_needed = seq_len + max_gen_len
-            past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
-
-        past_key_values = greedy_generate(
-            model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
-        )
-
-def load(model_name_or_path):
-    print(f"Loading model from {model_name_or_path} ...")
-    # however, tensor parallel for running falcon will occur bugs
+    # load model
+    print(f"Loading model from {args.model_name_or_path} ...")
     tokenizer = AutoTokenizer.from_pretrained(
-        model_name_or_path,
+        args.model_name_or_path,
         trust_remote_code=True,
+        chat_template=r"""
+{%- for message in messages %}
+    {%- if message['role'] == 'user' %}
+        {{- 'USER: ' + message['content'].strip() + '\n' }}
+    {%- elif message['role'] == 'assistant' %}
+        {{- 'ASSISTANT: '  + message['content'] + ' \n\n' }}
+    {%- endif %}
+{%- endfor %}
+{%- if add_generation_prompt %}
+    {{- 'ASSISTANT: ' }}
+{%- endif %}
+"""
     )
     model = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path,
+        args.model_name_or_path,
         device_map="auto",
         torch_dtype=torch.float16,
         trust_remote_code=True,
-        use_flash_attention_2=bool(os.environ.get('LCKV_FLASH_ATTN', False))
+        attn_implementation="flash_attention_2",
     )
-    if tokenizer.pad_token_id is None:
-        if tokenizer.eos_token_id is not None:
-            tokenizer.pad_token_id = tokenizer.eos_token_id
-        else:
-            tokenizer.pad_token_id = 0
-
     model.eval()
 
-    return model, tokenizer
+    # load data
+    print(f"Loading data from {args.data_root} ...")
+    mt_bench = Path(args.data_root) / "mt_bench.jsonl"
+    if not mt_bench.exists():
+        print("Downloading mt_bench data ...")
+        os.makedirs(args.data_root, exist_ok=True)
+        with open(mt_bench, "w") as f:
+            url = "https://raw.githubusercontent.com/lm-sys/FastChat/main/fastchat/llm_judge/data/mt_bench/question.jsonl"
+            response = requests.get(url)
+            f.write(response.text)
 
-def main(args):
-    model_name_or_path = args.model_name_or_path
-    model, tokenizer = load(model_name_or_path)
-    test_filepath = os.path.join(args.data_root, "mt_bench.jsonl")
-    print(f"Loading data from {test_filepath} ...")
-
-    if not os.path.exists(test_filepath):
-        download_url(
-            "https://raw.githubusercontent.com/lm-sys/FastChat/main/fastchat/llm_judge/data/mt_bench/question.jsonl",
-            args.data_root,
-        )
-        os.rename(os.path.join(args.data_root, "question.jsonl"), test_filepath)
-
-    list_data = load_jsonl(test_filepath)
     prompts = []
-    for sample in list_data:
-        prompts += sample["turns"]
+    with open(mt_bench, "r") as f:
+        for line in f:
+            prompts += json.loads(line)["turns"]
 
-    if args.enable_streaming:
-        kv_cache = enable_streaming_llm(
-            model, start_size=args.start_size, recent_size=args.recent_size
+    # streaming inference
+    kwargs = {}
+    if args.sink_cache:
+        kwargs["past_key_values"] = SinkCache(args.window_length, args.num_sink_tokens)
+    
+    chat_history = []
+    streamer = TextStreamer(tokenizer, skip_prompt=True)
+    for prompt in prompts:
+        new_prompt = {"role": "user", "content": prompt}
+        print(tokenizer.apply_chat_template([new_prompt], add_generation_prompt=True, tokenize=False), end="")
+        
+        chat_history.append(new_prompt)
+        input_ids = tokenizer.apply_chat_template(chat_history, add_generation_prompt=True, return_tensors="pt").to(model.device)
+
+        output_sequences = model.generate(
+            input_ids=input_ids,
+            max_new_tokens=args.length,
+            temperature=args.temperature,
+            top_k=args.k,
+            top_p=args.p,
+            repetition_penalty=args.repetition_penalty,
+            do_sample=True,
+            streamer=streamer,
+            **kwargs,
         )
-    else:
-        kv_cache = None
 
-    streaming_inference(
-        model,
-        tokenizer,
-        prompts,
-        kv_cache,
-    )
+        chat_history.append({"role": "assistant", "content": tokenizer.decode(output_sequences[0, input_ids.shape[-1]:], skip_special_tokens=True)})
+        print()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--model_name_or_path", type=str, default="lmsys/vicuna-13b-v1.3"
-    )
-    parser.add_argument("--data_root", type=str, default="data/")
-    parser.add_argument("--enable_streaming", action="store_true")
-    parser.add_argument("--start_size", type=int, default=4)
-    parser.add_argument("--recent_size", type=int, default=2000)
-    args = parser.parse_args()
-
-    main(args)
+    main()
