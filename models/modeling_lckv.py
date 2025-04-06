371fa4c9505da2c2aa53a3c7d0a9d9a98bfcdfae
whywhy-rtx3090 <43395692+why-in-Shanghaitech@users.noreply.github.com>
2025-04-06 15:09:32 +0800

    feat: implement sdpa (#9)
    I have noticed increasing attention on sdpa, so I implement this. After a long time working on other projects, I am not 100% sure about the correctness. Experiments show sdpa performs a little bit worse than flash attention 2.

--- models/modeling_lckv.py
+++ models/modeling_lckv.py
@@ -280,9 +280,114 @@ class LCKVLlamaFlashAttention2(LCKVLlamaAttention):
         return attn_output, attn_weights, past_key_value
 
 
+class LCKVLlamaSdpaAttention(LCKVLlamaAttention):
+    """
+    LCKV Attention may not need to initialize weights for the key and value projections.
+    """
+
+    # Adapted from LlamaAttention.forward
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[LayerCache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,  # will become mandatory in v4.46
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if output_attentions:
+            # TODO: Improve this warning with e.g. `model.config.attn_implementation = "manual"` once this is implemented.
+            logger.warning_once(
+                "LlamaModel is using LlamaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, "
+                'but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
+            )
+            return super().forward(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_value,
+                output_attentions=output_attentions,
+                use_cache=use_cache,
+                cache_position=cache_position,
+                position_embeddings=position_embeddings,
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+        cos, sin = position_embeddings
+
+        query_states = self.q_proj(hidden_states)
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        query_states = apply_rotary(query_states, cos, sin)
+
+        # compute key and value states
+        if self.layer_type.computes_kv:
+            key_states = self.k_proj(hidden_states)
+            value_states = self.v_proj(hidden_states)
+
+            key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+            value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+            key_states = apply_rotary(key_states, cos, sin)
+
+            if isinstance(past_key_value, Cache):
+                # sin and cos are specific to RoPE models; cache_position needed for the static cache
+                cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+                key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+            past_key_value.layer_set(self.layer_idx, key_states, value_states)
+
+        # get the cached key and value states
+        # if the layer attends to the top layers, there are two cases:
+        # 1. the query length is 1, in which case we will not do iterative updates. Therefore, the kv lacks the current
+        #    query length and we need to fill it with zeros.
+        # 2. the query length is greater than 1, in which case we will do iterative updates and the kv will have the
+        #    correct query length.
+        key_states, value_states = past_key_value.layer_get(
+            self.layer_type.attends_to,
+            zerofill=self.layer_type.attends_top and q_len == 1,
+        )
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        causal_mask = attention_mask
+        if attention_mask is not None:
+            causal_mask = causal_mask[:, :, :, : key_states.shape[-2]]
+
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and causal_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
+
+        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+        is_causal = True if causal_mask is None and q_len > 1 else False
+
+        attn_output = torch.nn.functional.scaled_dot_product_attention(
+            query_states,
+            key_states,
+            value_states,
+            attn_mask=causal_mask,
+            dropout_p=self.attention_dropout if self.training else 0.0,
+            is_causal=is_causal,
+        )
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, -1)
+
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
+
+
 LCKV_LLAMA_ATTENTION_CLASSES = {
     "eager": LCKVLlamaAttention,
     "flash_attention_2": LCKVLlamaFlashAttention2,
+    "sdpa": LCKVLlamaSdpaAttention,
 }
 
 
@@ -297,7 +402,7 @@ class LCKVLlamaPreTrainedModel(LlamaPreTrainedModel):
     supports_gradient_checkpointing = False # not tested yet
     _no_split_modules = ["LCKVLlamaDecoderLayer"]
     _supports_flash_attn_2 = True
-    _supports_sdpa = False
+    _supports_sdpa = True
 
 
 class LCKVLlamaModel(LCKVLlamaPreTrainedModel, LlamaModel):
